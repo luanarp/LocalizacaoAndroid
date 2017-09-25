c0bfa58abc39d5cedf38ae0f6498082d5cfaaef1
Aluno <henrick200@live.com>
2017-09-25 09:21:50 -0300

    grdhgf

--- app/src/main/java/com/example/aluno/localizacao/MainActivity.java
+++ app/src/main/java/com/example/aluno/localizacao/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.aluno.localizacao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button bt1;
+    TextView tv1;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt1 = (Button) findViewById(R.id.bt1);
+        tv1 = (TextView) findViewById(R.id.bt1);
+
+
     }
 }
