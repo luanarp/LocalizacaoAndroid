892c5d3861fccae60054cf0f22d3153e3b22834a
Aluno <luanarp.ifpr@gmail.com>
2017-09-28 11:58:32 -0300

    Terminei o projeto

--- app/src/main/java/com/example/aluno/localizacao/MainActivity.java
+++ app/src/main/java/com/example/aluno/localizacao/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.List;
@@ -27,8 +28,6 @@ public class MainActivity extends AppCompatActivity {
     private Location location;   // devolve a coordenada gps
     private LocationManager locationManager; //vai buscar qual o provedor de dispostitivo disponivel
 
-    private Address endereco; //trazer o nome da rua, do estado, telefone...
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,56 +35,45 @@ public class MainActivity extends AppCompatActivity {
 
         txtLatitude =(TextView) findViewById(R.id.txtLatitude);
         txtLongitude=(TextView) findViewById(R.id.txtLongitude);
-        txtLongitude=(TextView) findViewById(R.id.txtCidade);
-        txtLongitude=(TextView) findViewById(R.id.txtEstado);
-        txtLongitude=(TextView) findViewById(R.id.txtPais);
+
 
         double latitude =0.0;
         double longitude =0.0;
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED){
-        // solicitar permissão ao usuário
+
+        if( ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+
         }else{
-            locationManager=(LocationManager)
-                    getSystemService(Context.LOCATION_SERVICE);
-            location= locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        }
-        if (location !=null){
-            longitude = location.getLongitude();
-            latitude = location.getLatitude();
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+             if(location == null){
+                 location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+             }
+
+
+            if (location !=null){
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+
+                txtLongitude.setText("Longitude: " + longitude);
+                txtLatitude.setText("Latitude: " + latitude);
+            }
+
+
         }
 
-        txtLongitude.setText("Longitude: " + longitude);
-        txtLatitude.setText("Latitude: " + latitude);
-        try{
-            endereco = buscarEndereco(latitude, longitude);
 
-            txtCidade.setText("Cidade " + endereco.getLocality());
-            txtEstado.setText("Estado " + endereco.getSubAdminArea());
-            txtPais.setText("Pais..:"+ endereco.getCountryName());
-
-        } catch(IOException e){
-            Log.i("GPS", e.getMessage());
-        }
     }
-        public Address buscarEndereco(double latitude, double longitude) throws IOException{
 
-                Geocoder geocoder;
-                Address address = null;
-                List<Address> addresses;
-
-                geocoder = new Geocoder(getApplicationContext());
-
-                addresses=geocoder.getFromLocation(latitude, longitude,1);   //quantidade de endereços, no caso 1
-
-                if (addresses.size()>0){     //se existe algum endereço
-                    address = addresses.get(0);  //atribuo ao address
-
-                }
-                return address;
-
-        }
 
 }
 
